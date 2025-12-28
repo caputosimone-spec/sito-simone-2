fbf3652815f7c2624d6c9f24b82bed628bc87988
caputosimone-spec <caputo.simone@majoranamaitani.edu.it>
2025-12-28 19:34:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Messaggio di benvenuto quando la pagina è pronta
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Sito caricato correttamente 🚀");
+});
+
+// Interazione con il pulsante
+const button = document.querySelector(".button");
+
+button.addEventListener("click", () => {
+  alert("Stai per lasciare il sito di Simone 😉");
+});
